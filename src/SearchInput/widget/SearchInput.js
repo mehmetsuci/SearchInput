0a304c07ed73405675dda9dc793fbbfdf6f5d415
Jelte Lagendijk <jelte.lagendijk@mendix.com>
2016-04-12 14:33:39 +0200

    Code cleanup

--- src/SearchInput/widget/SearchInput.js
+++ src/SearchInput/widget/SearchInput.js
@@ -1,197 +1,199 @@
 define([
-	"dojo/_base/declare",
-	"mxui/widget/_WidgetBase",
-	"dijit/_TemplatedMixin",
-	"mxui/dom",
-	"dojo/dom",
-	"dojo/dom-style",
-	"dojo/dom-construct",
-	"dojo/_base/array",
-	"dojo/_base/lang",
-	"dojo/keys",
-	"dojo/html",
-	"dojo/dom-prop",
-	"dojo/text!SearchInput/widget/template/SearchInput.html"
-], function(declare, _WidgetBase, _TemplatedMixin, dom, dojoDom, dojoStyle, dojoConstruct, dojoArray, dojoLang, dojoKeys, dojoHtml, domProp, widgetTemplate) {
-	"use strict";
+    "dojo/_base/declare",
+    "mxui/widget/_WidgetBase",
+    "dijit/_TemplatedMixin",
+    "dojo/dom-style",
+    "dojo/dom-construct",
+    "dojo/_base/array",
+    "dojo/_base/lang",
+    "dojo/keys",
+    "dojo/html",
+    "dojo/dom-prop",
+    "dojo/text!SearchInput/widget/template/SearchInput.html"
+], function(declare, _WidgetBase, _TemplatedMixin, dojoStyle, dojoConstruct, dojoArray, dojoLang, dojoKeys, dojoHtml, domProp, widgetTemplate) {
+    "use strict";
 
-	return declare("SearchInput.widget.SearchInput", [_WidgetBase, _TemplatedMixin], {
-		templateString: widgetTemplate,
+    return declare("SearchInput.widget.SearchInput", [_WidgetBase, _TemplatedMixin], {
+        templateString: widgetTemplate,
 
-    searchFormNode: null,
-		searchSelectNode: null,
-		searchInputNode: null,
-		infoTextNode: null,
+        searchFormNode: null,
+        searchSelectNode: null,
+        searchInputNode: null,
+        infoTextNode: null,
 
-		targetAttribute: "",
+        targetAttribute: "",
 
-		_handles: null,
-		_contextObj: null,
-		_alertDiv: null,
+        _handles: null,
+        _contextObj: null,
+        _alertDiv: null,
 
-		postCreate: function() {
+        postCreate: function() {
+            logger.debug(this.id + ".postCreate");
+            this._updateRendering();
+            this._setupEvents();
 
-			this._updateRendering();
-			this._setupEvents();
+            if (this.placeholder) {
+                domProp.set(this.searchInputNode, "placeholder", this.placeholder);
+            }
+        },
 
-			if (this.placeholder) {
-				domProp.set(this.searchInputNode, "placeholder", this.placeholder);
-			}
-		},
+        update: function(obj, callback) {
+            logger.debug(this.id + ".update");
+            this._contextObj = obj;
 
-		update: function(obj, callback) {
+            if (this._contextObj) {
+                this._resetSubscriptions();
+                this._updateRendering(callback);
+            } else {
+                mendix.lang.nullExec(callback);
+            }
+        },
 
-			this._contextObj = obj;
+        _setupEvents: function() {
+            logger.debug(this.id + "._setupEvents");
+            this.connect(this.searchInputNode, "onkeydown", dojoLang.hitch(this, this.onKeyDown));
+            this.connect(this.searchFormNode, "submit", dojoLang.hitch(this, this.onSubmit));
+            this.connect(this.searchSelectNode, "onclick", dojoLang.hitch(this, function(e) {
+                this.executeMicroflow(this.mfToExecute);
+            }));
 
-			if (this._contextObj) {
-				this._resetSubscriptions();
-				this._updateRendering(callback);
-			}
-			else {
-				// Try to execute as function, otherwise fail silently
-				mendix.lang.nullExec(callback);
-			}
-		},
+        },
 
-		_setupEvents: function() {
+        _updateRendering: function(callback) {
+            logger.debug(this.id + "._updateRendering");
+            if (this._contextObj !== null) {
+                dojoStyle.set(this.domNode, "display", "block");
 
-			this.connect(this.searchInputNode, "onkeydown", dojoLang.hitch(this, this.onKeyDown));
-			this.connect(this.searchFormNode, "submit", dojoLang.hitch(this, this.onSubmit));
-			this.connect(this.searchSelectNode, "onclick", dojoLang.hitch(this, function(e) {
-				this.executeMicroflow(this.mfToExecute);
-			}));
+                var targetAttributeValue = this._contextObj.get(this.targetAttribute);
 
-		},
+                this.searchInputNode.value = targetAttributeValue;
 
-		_updateRendering: function(callback) {
-			if (this._contextObj !== null) {
-				dojoStyle.set(this.domNode, "display", "block");
+            } else {
+                dojoStyle.set(this.domNode, "display", "none");
+            }
 
-				var targetAttributeValue = this._contextObj.get(this.targetAttribute);
+            // Important to clear all validations!
+            this._clearValidations();
 
-				this.searchInputNode.value = targetAttributeValue;
+            mendix.lang.nullExec(callback);
+        },
 
-			} else {
-				dojoStyle.set(this.domNode, "display", "none");
-			}
+        onSubmit: function (e) {
+            logger.debug(this.id + ".onSubmit");
+            e.preventDefault();
+            return false;
+        },
 
-			// Important to clear all validations!
-			this._clearValidations();
+        onKeyDown: function(event) {
+            if (event.keyCode === dojoKeys.ENTER) {
+                this._contextObj.set(this.targetAttribute, this.searchInputNode.value);
+                event.preventDefault();
+                if (this.mfToExecute !== "") {
+                    this.executeMicroflow(this.mfToExecute);
+                }
+            }
+        },
 
-			mendix.lang.nullExec(callback);
-		},
+        executeMicroflow: function(mf) {
+            logger.debug(this.id + ".executeMicroFlow");
+            if (mf && this._contextObj) {
+                this._contextObj.set(this.targetAttribute, this.searchInputNode.value);
+                mx.data.action({
+                    store: {
+                        caller: this.mxform
+                    },
+                    params: {
+                        actionname: mf,
+                        applyto: "selection",
+                        guids: [this._contextObj.getGuid()]
+                    },
+                    callback: function() {
+                    },
+                    error: dojoLang.hitch(this, function() {
+                        logger.error(this.id + ".executeMicroFlow: XAS error executing microflow");
+                    })
+                });
+            }
+        },
 
-    onSubmit: function (e) {
-      e.preventDefault();
-      return false;
-    },
+        _handleValidation: function(validations) {
+            logger.debug(this.id + "._handleValidation");
+            this._clearValidations();
 
-		onKeyDown: function(event) {
-			if (event.keyCode == dojoKeys.ENTER) {
-				this._contextObj.set(this.targetAttribute, this.searchInputNode.value);
-				event.preventDefault();
-				if (this.mfToExecute !== "") {
-					this.executeMicroflow(this.mfToExecute);
-				}
-			}
-		},
+            var validation = validations[0],
+                message = validation.getReasonByAttribute(this.targetAttribute);
 
+            if (this.readOnly) {
+                validation.removeAttribute(this.targetAttribute);
+            } else if (message) {
+                this._addValidation(message);
+                validation.removeAttribute(this.targetAttribute);
+            }
+        },
 
-		executeMicroflow: function(mf) {
-			this._contextObj.set(this.targetAttribute, this.searchInputNode.value);
-			if (mf && this._contextObj) {
-				mx.data.action({
-					store: {
-						caller: this.mxform
-					},
-					params: {
-						actionname: mf,
-						applyto: "selection",
-						guids: [this._contextObj.getGuid()],
+        _clearValidations: function() {
+            logger.debug(this.id + "._clearValidations");
+            dojoConstruct.destroy(this._alertDiv);
+            this._alertDiv = null;
+        },
 
-					},
-					callback: function() {
-					},
-					error: dojoLang.hitch(this, function() {
-						logger.error(this.id + ".executeMicroFlow: XAS error executing microflow");
-					})
-				});
-			}
-		},
+        _showError: function(message) {
+            logger.debug(this.id + "._showError");
+            if (this._alertDiv !== null) {
+                dojoHtml.set(this._alertDiv, message);
+                return true;
+            }
+            this._alertDiv = dojoConstruct.create("div", {
+                "class": "alert alert-danger",
+                "innerHTML": message
+            });
+            dojoConstruct.place(this.domNode, this._alertDiv);
+        },
 
-		_handleValidation: function(validations) {
-			this._clearValidations();
+        _addValidation: function(message) {
+            logger.debug(this.id + "._addValidation");
+            this._showError(message);
+        },
 
-			var validation = validations[0],
-				message = validation.getReasonByAttribute(this.targetAttribute);
+        _resetSubscriptions: function() {
+            logger.debug(this.id + "._resetSubscriptions");
+            // Release handles on previous object, if any.
+            if (this._handles) {
+                dojoArray.forEach(this._handles, function(handle) {
+                    mx.data.unsubscribe(handle);
+                });
+                this._handles = [];
+            }
 
-			if (this.readOnly) {
-				validation.removeAttribute(this.targetAttribute);
-			} else if (message) {
-				this._addValidation(message);
-				validation.removeAttribute(this.targetAttribute);
-			}
-		},
+            // When a mendix object exists create subscribtions.
+            if (this._contextObj) {
+                var objectHandle = this.subscribe({
+                    guid: this._contextObj.getGuid(),
+                    callback: dojoLang.hitch(this, function(guid) {
+                        this._updateRendering();
+                    })
+                });
 
-		_clearValidations: function() {
-			dojoConstruct.destroy(this._alertDiv);
-			this._alertDiv = null;
-		},
+                var attrHandle = this.subscribe({
+                    guid: this._contextObj.getGuid(),
+                    attr: this.backgroundColor,
+                    callback: dojoLang.hitch(this, function(guid, attr, attrValue) {
+                        this._updateRendering();
+                    })
+                });
 
-		_showError: function(message) {
-			if (this._alertDiv !== null) {
-				dojoHtml.set(this._alertDiv, message);
-				return true;
-			}
-			this._alertDiv = dojoConstruct.create("div", {
-				"class": "alert alert-danger",
-				"innerHTML": message
-			});
-			dojoConstruct.place(this.domNode, this._alertDiv);
-		},
+                var validationHandle = this.subscribe({
+                    guid: this._contextObj.getGuid(),
+                    val: true,
+                    callback: dojoLang.hitch(this, this._handleValidation)
+                });
 
-		_addValidation: function(message) {
-			this._showError(message);
-		},
-
-		_resetSubscriptions: function() {
-			// Release handles on previous object, if any.
-			if (this._handles) {
-				dojoArray.forEach(this._handles, function(handle) {
-					mx.data.unsubscribe(handle);
-				});
-				this._handles = [];
-			}
-
-			// When a mendix object exists create subscribtions.
-			if (this._contextObj) {
-				var objectHandle = this.subscribe({
-					guid: this._contextObj.getGuid(),
-					callback: dojoLang.hitch(this, function(guid) {
-						this._updateRendering();
-					})
-				});
-
-				var attrHandle = this.subscribe({
-					guid: this._contextObj.getGuid(),
-					attr: this.backgroundColor,
-					callback: dojoLang.hitch(this, function(guid, attr, attrValue) {
-						this._updateRendering();
-					})
-				});
-
-				var validationHandle = this.subscribe({
-					guid: this._contextObj.getGuid(),
-					val: true,
-					callback: dojoLang.hitch(this, this._handleValidation)
-				});
-
-				this._handles = [objectHandle, attrHandle, validationHandle];
-			}
-		}
-	});
+                this._handles = [objectHandle, attrHandle, validationHandle];
+            }
+        }
+    });
 });
 
 require(["SearchInput/widget/SearchInput"], function() {
-	"use strict";
+    "use strict";
 });
